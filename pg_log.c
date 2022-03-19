041cb5e2bd6c830f68f929cca298de63cb48d983
Pierre <pierre.forstmann@free.fr>
2022-03-19 18:07:22 +0100

    when building new buf_v1 make sure variable i starts with 0 and not with 1

--- pg_log.c
+++ pg_log.c
@@ -196,7 +196,7 @@ static Datum pg_log_internal(FunctionCallInfo fcinfo)
                 {
 			buf_v1[i] = '\0';
 			line_count++;
-			i = 0;
+			i = -1;
 
 			values[0] = buf_v1;
 			tuple = BuildTupleFromCStrings(attinmeta, values);
